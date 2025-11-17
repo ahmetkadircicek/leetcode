039b1e8fd9993884101b6f5f371d2f0a204ac31c
Ahmet K. <104458700+ahmetkadircicek@users.noreply.github.com>
2025-11-17 18:38:00 +0300

    fix: correct kLengthApart logic by validating spacing only on 1s and ensuring proper gap check

--- counter.js
+++ counter.js
@@ -1,12 +1,5 @@
-currentNumber = 0;
-function counter(number) {
-    if (currentNumber === 0) {
-        currentNumber = number;
-    } else {
-        currentNumber += 1;
-    }
-    return currentNumber;
-}
-
-console.log(counter(10));
-console.log(counter(20));
+var createCounter = function(n) {
+    return function() {
+        return n++;
+    };
+};
